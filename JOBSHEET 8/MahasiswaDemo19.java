4df657f4a54f2fc7c5abe8e1731788de8d98cf03
JustinLaiya <faesalgali@gmail.com>
2025-05-27 20:03:32 +0700

    asd

---
+++ JOBSHEET 8/MahasiswaDemo19.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class MahasiswaDemo19 {
+
+    public static void main(String[] args) {
+        StackTugasMahasiswa19 stack = new StackTugasMahasiswa19(5);
+        Scanner scan = new Scanner(System.in);
+        int pilih;
+        do {
+            System.out.println("Pilih Menu");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Lihat Tugas Teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.print("Pilih : ");
+            pilih = scan.nextInt();
+            scan.nextLine();
+            switch (pilih) {
+                case 1 :
+                    System.out.print("Nama :");
+                    String nama = scan.nextLine();
+                    System.out.print("Nim :");
+                    String nim = scan.nextLine();
+                    System.out.print ("Kelas:");
+                    String kelas = scan.nextLine();
+                    Mahasiswa19 mhs = new Mahasiswa19 (nama,nim,kelas);
+                    stack.push(mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n",  mhs.nama);
+                    break;
+                case 2 :
+                    Mahasiswa19 dinilai = stack.pop ();
+                    if (dinilai != null) {
+                        System.out.println("Menilai Tugas dari " +dinilai.nama);
+                        System.out.println("Masukan nilai (0-100) : ");
+                        int nilai = scan.nextInt();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai Tugas %s = %d\n", dinilai.nama, nilai);
+                    }
+                    break;
+                case 3 :
+                    Mahasiswa19 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.println("Tugas Terakhir dikumpulkan oleh " + lihat.nama);
+                    }
+                    break;
+                case 4 :
+                    System.out.println("Daftar semua tugas");
+                    System.out.println("Nama\tNIM\tKelas");
+                    stack.print();
+                    break;
+                default :
+                System.out.println("Pilihan tidak ada");
+            }
+        } while (pilih >= 1 && pilih <=4);
+    }
+
+}
---
+++ JOBSHEET 8/StackTugasMahasiswa19.java
@@ -0,0 +1,66 @@
+public class StackTugasMahasiswa19 {
+    //ATRIBUT
+    Mahasiswa19[] stack;
+    int top;
+    int size;
+
+    public StackTugasMahasiswa19(int size) {
+        this.size = size;
+        stack = new Mahasiswa19[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size -1) {
+            return true;
+        } else { 
+            return false;
+        }
+    }
+
+    public boolean isEmpty () {
+        if (top== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(Mahasiswa19 mhs) {
+        if (!isFull()) {
+            top ++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack sudah penuh");
+        }
+    }
+
+    public Mahasiswa19 pop() {
+        if (!isEmpty()) {
+            Mahasiswa19 m = stack[top];
+            top--;
+            return m;
+    } else {
+        System.out.println("Stack sudah kosong");
+        return null;
+        }
+    }
+
+    public Mahasiswa19 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+    } else {
+        System.out.println("Stack kosong! tidak ada tugas yang dikumpulkan");
+        return null;
+        }
+    }
+
+    public void print(){
+    for (int i = top; i >= 0; i--) {
+        System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+    }
+    System.out.println();
+}
+
+
+} 
